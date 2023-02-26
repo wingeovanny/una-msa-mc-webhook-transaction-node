4c2c70e14c10cd4fe8d937536a05107b2d37f6fd
Geovanny Bucay <ebucayle@pichincha.com>
2023-02-26 13:19:09 -0500

    add controller

--- src/webhook-client-service.module.ts
+++ src/webhook-client-service.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import configuration from './config/service-configuration';
 import { LoggerModule } from '@deuna/node-logger-lib';
 import { ALL_EXCEPTION_FILTERS_FOR_PROVIDER } from '@deuna/node-shared-lib';
+import { MetaServiceModule } from './modules/meta-service/meta-service.module';
+import { NotificationClientTransactionModule } from './modules/notification-client-transaction/notification-client-transaction.module';
 
 @Module({
   imports: [
@@ -11,6 +13,8 @@ import { ALL_EXCEPTION_FILTERS_FOR_PROVIDER } from '@deuna/node-shared-lib';
       isGlobal: true,
       load: [configuration],
     }),
+    MetaServiceModule,
+    NotificationClientTransactionModule,
     LoggerModule.forRoot({ context: 'WebHook Client Service' }),
   ],
   providers: [...ALL_EXCEPTION_FILTERS_FOR_PROVIDER],
